b75a0f6e1a54fc66abdf0eb21a08a1f96299b978
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-21 02:02:02 +0330

    Fix: Apply Model

--- database/migrations/2024_09_20_191449_create_applies_table.php
+++ database/migrations/2024_09_20_191449_create_applies_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('applies', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('offer_id')->constrained('offers');
+            $table->string('resume_doc')->nullable();
+            $table->longText('resume_text')->nullable();
+            $table->integer('status')->default(1); //1=>applied , 2=>reject , 3=>next Level ,...
+            $table->string('url')->nullable();
+            $table->string('feedback')->nullable();
+            $table->timestamp('applied_at')->nullable();
             $table->timestamps();
         });
     }
