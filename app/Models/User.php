ea51170ae0030a3cb3e73665c827ca2831e1bfe4
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-20 23:00:47 +0330

    Fix: User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
+        'username',
         'password',
+        'photo',
+        'birthdate',
     ];
 
     /**
