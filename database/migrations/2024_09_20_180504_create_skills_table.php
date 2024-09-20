a66c4d2554ab1ef8f529e07983263e135fda6033
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-21 01:38:33 +0330

    Fix: Skill Model

--- database/migrations/2024_09_20_180504_create_skills_table.php
+++ database/migrations/2024_09_20_180504_create_skills_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->integer('type');//hard , soft
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
