f5f00685315beddea867236a770422aa5f770ff0
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-21 01:33:17 +0330

    Fix: Country Model

--- database/migrations/2024_09_20_172620_create_countries_table.php
+++ database/migrations/2024_09_20_172620_create_countries_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('continent')->nullable();
+            $table->string('language')->nullable();
             $table->timestamps();
         });
     }
