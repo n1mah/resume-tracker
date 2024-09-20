cfe6f0f1579cc74504ce6f833ed3d4b04b87384f
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-21 02:20:34 +0330

    Feat: Add Relation between Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -38,7 +39,7 @@ class User extends Authenticatable
         return $this->hasMany(Offer::class);
     }
 
-    public function apply():HasMany
+    public function apply():BelongsToMany
     {
         return $this->belongsToMany(Apply::class);
     }
