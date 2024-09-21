d11eea9712bb94cc4955a2ebdf092de2e6815ef0
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-21 23:36:02 +0330

    Fix: Update Country Page

--- resources/js/Pages/Dashboard/Country/Index.jsx
+++ resources/js/Pages/Dashboard/Country/Index.jsx
@@ -8,7 +8,51 @@ function Index() {
         >
             <Head title="Country" />
 
-            <div className="py-12">
+            <div className="w-10/12 mx-auto py-12">
+
+                <div className={'w-full my-2 px-1'}>
+                    <button className={'px-3 py-1.5 bg-gray-800 text-white rounded-2xl'}>Add Country</button>
+                </div>
+
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                        <thead
+                            className="text-xs text-gray-700 uppercase bg-gray-300">
+                        <tr>
+                            <th scope="col" className="px-6 py-5">
+                                Name
+                            </th>
+                            <th scope="col" className="px-6 py-5">
+                                Continent
+                            </th>
+                            <th scope="col" className="px-6 py-5">
+                                Language
+                            </th>
+                            <th scope="col" className="px-6 py-5">
+                                <span className="sr-only">Edit</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr className="bg-white border-b  hover:bg-gray-50 ">
+                            <th scope="row"
+                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                Iran
+                            </th>
+                            <td className="px-6 py-4">
+                                Asia
+                            </td>
+                            <td className="px-6 py-4">
+                                Persian
+                            </td>
+                            <td className="px-6 py-4 text-right">
+                                <a href="#"
+                                   className="font-medium text-blue-600  hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
 
             </div>
         </AuthenticatedLayout>
