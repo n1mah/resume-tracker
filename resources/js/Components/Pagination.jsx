04f43982273ee52fce60e18c928bc8f45ce99d6f
Nima Heidari <nima.heidari.n@gmail.com>
2024-09-22 12:59:33 +0330

    Fix(style): Update Pagination Component

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -3,11 +3,11 @@ export default function Pagination({links}) {
     if (links)
     return (
         <div>
-            <ul className="pagination flex gap-x-2 justify-center">
+            <ul className="pagination flex justify-center">
                 {links.map((link,key) => (
                     <li key={key} className="page-item">
                         <Link
-                            className={ (link.active ? 'inline-flex bg-gray-300 px-3 py-1.5':' inline-flex px-3 py-1.5 ') + (!link.url?'opacity-25':'') }
+                            className={ (link.active ? 'inline-flex bg-gray-300 px-3 py-1.5':' inline-flex px-3 py-1.5 ') + (!link.url?'opacity-25 cursor-no-drop':'') }
                             href={link.url || ''} dangerouslySetInnerHTML={{__html:link.label}}></Link>
                     </li>
                 ))}
