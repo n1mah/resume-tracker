6fb3c6cb5b352387014901b166da2d7925d309b9
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-13 23:26:21 +0330

    Fix: Re-Update offer model

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -28,9 +28,9 @@ class Offer extends Model
         'is_visa_needed',
         'is_authorized',
         'is_living',
-            'reference',
-            'added_at',
-            'is_active'
+        'reference',
+        'added_at',
+        'is_active'
     ];
     public function user(): BelongsTo{
         return $this->belongsTo(User::class);
